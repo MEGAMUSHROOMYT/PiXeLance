156ce33d1937e818b942d96c46069ca5a4d6ddaa
sheezzmee <ssewess01@gmail.com>
2023-09-03 19:09:54 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import packageJSON from '../package.json'
 
 switch (location.href) {
     case 'https://tankionline.com/ru/':
+    case 'https://tankionline.com/ru/#':
         replaceHTML(
             `${packageJSON.usercontent}/website/tankionline.com.ru.html`,
             `${packageJSON.usercontent}/website/css/style_ru.css`
@@ -11,6 +12,7 @@ switch (location.href) {
         break;
 
     case 'https://tankionline.com/en/':
+    case 'https://tankionline.com/en/#':
         replaceHTML(
             `${packageJSON.usercontent}/website/tankionline.com.en.html`,
             `${packageJSON.usercontent}/website/css/style.css`
